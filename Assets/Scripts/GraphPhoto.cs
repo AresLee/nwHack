812114db104c0694a5dff9c9874f30b22f519e33
Lee Wasilenko <lee.wasilenko@gmail.com>
2015-03-14 04:56:46 -0700

    Add method to smoothly move GraphPhoto from point a to point b and back again

--- Assets/Scripts/GraphPhoto.cs
+++ Assets/Scripts/GraphPhoto.cs
@@ -3,13 +3,35 @@ using System.Collections;
 
 public class GraphPhoto : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public string description; 
+	public Vector3 pointB;
 	
+	IEnumerator Start () {
+		Vector3 pointA = transform.position;
+		while (true) {
+			yield return StartCoroutine(MoveObject(transform, pointA, pointB, 3.0f));
+			yield return StartCoroutine(MoveObject(transform, pointB, pointA, 3.0f));
+		}
 	}
+	
+	IEnumerator MoveObject (Transform thisTransform, Vector3 startPos, Vector3 endPos, float time) {
+		float i = 0.0f;
+		float rate = 1.0f / time;
+		while (i < 1.0f) {
+			i += Time.deltaTime * rate;
+			thisTransform.position = Vector3.Lerp(startPos, endPos, i);
+			yield return null; 
+		}
+	}
+
+
+	// Use this for initialization
+//	void Start () {
+//	
+//	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
