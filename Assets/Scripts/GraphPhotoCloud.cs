5c00710e188754be357a68ed50594127e96957c4
Lee Wasilenko <lee.wasilenko@gmail.com>
2015-03-15 00:56:17 -0700

    Add method to distribute photos evenly on a sphere

--- Assets/Scripts/GraphPhotoCloud.cs
+++ Assets/Scripts/GraphPhotoCloud.cs
@@ -6,7 +6,8 @@ using System;
 public class GraphPhotoCloud : MonoBehaviour, IGazeable {
 
 	List<GameObject> graphPhotos;
-	private int maxPhotosInRow = 12;
+	private int sampleSize = 12;
+	public float photoSpacing = 3.0f;
 
 	void Start () {
 		graphPhotos = populateGraphList ();	
@@ -19,23 +20,34 @@ public class GraphPhotoCloud : MonoBehaviour, IGazeable {
 	List<GameObject> populateGraphList ()
 	{
 		graphPhotos = new List<GameObject> ();
-		for (int i = 0; i < maxPhotosInRow; i++) {
+		for (int i = 0; i < sampleSize; i++) {
 			GameObject photo = (GameObject)Instantiate(Resources.Load("GraphPhoto"));
 			photo.transform.parent = GameObject.Find ("GraphPhotoCloud").transform;
-			photo.transform.position =  positionAroundCircle(i, photo); //new Vector3(i*photo.renderer.bounds.size.x,0,-7);
+			photo.transform.position =  distributePhotosOnSphere(i, photo);
 			graphPhotos.Add(photo);
 		}
 		return graphPhotos;
 	}
 
-	Vector3 positionAroundCircle (int i, GameObject unit)
+	Vector3 distributePhotosOnSphere (int i, GameObject photo)
 	{	
-		float radius = unit.renderer.bounds.size.magnitude * maxPhotosInRow / 3;
-		float angle = i * 2*Mathf.PI / maxPhotosInRow;
+		float inc =  Mathf.PI  * (3 - Mathf.Sqrt(5));
+		float off = 2 / sampleSize;
+		float y = i * off - 1 + (off / 2);
+		float radius = Mathf.Sqrt(1 - y*y);
+		//float radius = photo.renderer.bounds.size.magnitude * maxPhotosInRow / photoSpacing;
+		float phi = i * inc;
+		return new Vector3((Mathf.Cos(phi)*radius), y, Mathf.Sin(phi)*radius);
+	}
+
+	Vector3 distributePhotosOnCircle (int i, GameObject photo)
+	{	
+		float radius = photo.renderer.bounds.size.magnitude * sampleSize / photoSpacing;
+		float theta = i * 2*Mathf.PI / sampleSize;
 		Vector3 pos = new Vector3();
-		pos.x = transform.position.x + radius*Mathf.Sin(angle); 
+		pos.x = transform.position.x + radius*Mathf.Sin(theta); 
 		pos.y = transform.position.y;
-		pos.z = transform.position.z + radius*Mathf.Cos(angle); 
+		pos.z = transform.position.z + radius*Mathf.Cos(theta); 
 		return pos;
 	}
 
