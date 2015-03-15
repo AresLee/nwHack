6340e5b8239d4c578f41563d683e8a84fd63e2bf
Lee Wasilenko <lee.wasilenko@gmail.com>
2015-03-14 19:07:18 -0700

    Photos spawn evenly spaced  around the centre of the GraphPhotoCloud

--- Assets/GraphPhotoCloud.cs
+++ Assets/GraphPhotoCloud.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 public class GraphPhotoCloud : MonoBehaviour {
 
@@ -25,9 +26,20 @@ public class GraphPhotoCloud : MonoBehaviour {
 		for (int i = 0; i < maxPhotosInRow; i++) {
 			GameObject photo = (GameObject)Instantiate(Resources.Load("GraphPhoto"));
 			photo.transform.parent = GameObject.Find ("GraphPhotoCloud").transform;
-			photo.transform.position = new Vector3(i*photo.renderer.bounds.size.x,0,-7);
+			photo.transform.position =  positionAroundCircle(i, photo); //new Vector3(i*photo.renderer.bounds.size.x,0,-7);
 			graphPhotos.Add(photo);
 		}
 		return graphPhotos;
+	}
+
+	Vector3 positionAroundCircle (int i, GameObject unit)
+	{	
+		float radius = unit.renderer.bounds.size.magnitude * maxPhotosInRow / 2;
+		float angle = i * 2*Mathf.PI / maxPhotosInRow;
+		Vector3 pos = new Vector3();
+		pos.x = transform.position.x + radius*Mathf.Sin(angle); 
+		pos.y = transform.position.y;
+		pos.z = transform.position.z + radius*Mathf.Cos(angle); 
+		return pos;
 	}
 }
