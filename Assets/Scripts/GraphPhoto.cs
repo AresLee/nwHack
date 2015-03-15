f2f17b2ed04e0db24e0d6cb63b4dba373bcbdf4f
Lee Wasilenko <lee.wasilenko@gmail.com>
2015-03-15 08:35:49 -0700

    Move hammer around

--- Assets/Scripts/GraphPhoto.cs
+++ Assets/Scripts/GraphPhoto.cs
@@ -1,6 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using VibrationType = Thalmic.Myo.VibrationType;
+
 public class GraphPhoto : MonoBehaviour, IGazeable {
 
 	private Vector3 startPos;
@@ -9,12 +14,15 @@ public class GraphPhoto : MonoBehaviour, IGazeable {
 	public float speed = 12.0f;
 	public int index;
 	private bool isGazedAt = false;
-
+	public GameObject myo = null;
+	private Pose _lastPose = Pose.Unknown;
 	GameObject camera;
+
 
 
 	void Start () {
 		camera = GameObject.FindGameObjectWithTag ("MainCamera");
+		myo = GameObject.FindGameObjectWithTag ("Myo");
 		startPos = transform.position;
 		GraphPhotoCloud graphPhotoCloud = (GraphPhotoCloud)GameObject.FindObjectOfType (typeof(GraphPhotoCloud));
 		endPos = graphPhotoCloud.calculatePositionOnFibonacciSphere(this, 8.0f);
@@ -22,16 +30,56 @@ public class GraphPhoto : MonoBehaviour, IGazeable {
 
 	void Update () {
 		float step = speed * Time.deltaTime;
+		ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo> ();
+		if (thalmicMyo.pose != _lastPose) {
+			_lastPose = thalmicMyo.pose;
+			
+			// Vibrate the Myo armband when a fist is made.
+			if (thalmicMyo.pose == Pose.Fist) {
+				thalmicMyo.Vibrate (VibrationType.Medium);
+				
+				ExtendUnlockAndNotifyUserAction (thalmicMyo);
+				
+			} else if (thalmicMyo.pose == Pose.WaveIn) {
+				if (isGazedAt) {
+					//transform.position = Vector3.MoveTowards (transform.position, endPos, step*2);
+					transform.position =  endPos;
+				}
+
+				ExtendUnlockAndNotifyUserAction (thalmicMyo);
+			} else if (thalmicMyo.pose == Pose.WaveOut) {
+				if(transform.position != startPos) {
+					//transform.position = Vector3.MoveTowards (transform.position, startPos, step);
+					transform.position =  startPos;
+				}
+				
+				ExtendUnlockAndNotifyUserAction (thalmicMyo);
+			} else if (thalmicMyo.pose == Pose.DoubleTap) {
+
+				
+				ExtendUnlockAndNotifyUserAction (thalmicMyo);
+			}
+		}
+		
 		if (isGazedAt) {
-			//transform.position = Vector3.MoveTowards (transform.position, endPos, step*2);
 			transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
 		} else {
-			if(transform.position != startPos) {
-				transform.position = Vector3.MoveTowards (transform.position, startPos, step);
-			}
 			transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
 		}
 	}	
+
+	// Extend the unlock if ThalmcHub's locking policy is standard, and notifies the given myo that a user action was
+    // recognized.
+    void ExtendUnlockAndNotifyUserAction (ThalmicMyo myo)
+    {
+        ThalmicHub hub = ThalmicHub.instance;
+
+        if (hub.lockingPolicy == LockingPolicy.Standard) {
+            myo.Unlock (UnlockType.Timed);
+        }
+
+        myo.NotifyUserAction ();
+    }
 
 	public void reactToGaze() {
 		isGazedAt = true;
