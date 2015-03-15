3dd8a4f8ca3bb54e55d9267abd06bf7eef5d4182
Lee Wasilenko <lee.wasilenko@gmail.com>
2015-03-14 23:09:17 -0700

    Remove yield and concurrency calls

--- Assets/Scripts/GraphPhoto.cs
+++ Assets/Scripts/GraphPhoto.cs
@@ -7,24 +7,21 @@ public class GraphPhoto : MonoBehaviour, IGazeable {
 	public Vector3 pointB;
 	private bool isGazedAt = false;
 
-	IEnumerator Start () {
+	void Start () {
 		Vector3 pointA = transform.position;
-		while (true) {
 			if (isGazedAt) {
-				yield return StartCoroutine (MoveObject (transform, pointA, pointB, 3.0f));
+				MoveObject (transform, pointA, pointB, 3.0f);
 			} else {
-				yield return StartCoroutine (MoveObject (transform, pointB, pointA, 3.0f));
+				MoveObject (transform, pointB, pointA, 3.0f);
 			}
-		}
 	}
 	
-	IEnumerator MoveObject (Transform thisTransform, Vector3 startPos, Vector3 endPos, float time) {
+	void MoveObject (Transform thisTransform, Vector3 startPos, Vector3 endPos, float time) {
 		float i = 0.0f;
 		float rate = 1.0f / time;
 		while (i < 1.0f) {
 			i += Time.deltaTime * rate;
 			thisTransform.position = Vector3.Lerp(startPos, endPos, i);
-			yield return null; 
 		}
 	}
 
